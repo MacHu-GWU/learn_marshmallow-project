bcd70a557627a3ecb8b9b4b554ce58f7cfd668bd
MacHu-GWU <husanhe@gmail.com>
2016-08-01 10:09:40 -0400

    2016-08-01

--- learn_marshmallow/schema/pre_post_processing.py
+++ learn_marshmallow/schema/pre_post_processing.py
@@ -3,6 +3,10 @@
 
 """
 请注意pre_load, pre_dump, post_load, post_dump是Schema级的, 不是field级的。
+
+Ref:
+
+- http://marshmallow.readthedocs.io/en/latest/extending.html?highlight=pre_load#pre-processing-and-post-processing-methods
 """
 
 from __future__ import print_function
